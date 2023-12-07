1bf2c3ef51cba841d211050c51a6cae50e1de4bf
Tarre <tarreng@gmail.com>
2023-12-07 10:10:05 +0100

    Adding text for test

--- src/FourClient.java
+++ src/FourClient.java
@@ -1,2 +1,5 @@
 public class FourClient {
+    public static void main(String[] args) {
+        System.out.println("Hello Client!");
+    }
 }
--- src/FourServer.java
+++ src/FourServer.java
@@ -1,2 +1,5 @@
 public class FourServer {
+    public static void main(String[] args) {
+        System.out.println("Hello Server!");
+    }
 }
